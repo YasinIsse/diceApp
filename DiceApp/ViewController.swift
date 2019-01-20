04e681b30cfc090588af4b941af96746fa142351
Yasin Isse <isse.yasin@gmail.com>
2019-01-19 19:07:36 -0600

    Initial commit

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
 
+    @IBAction func rollButtonPress(_ sender: Any) {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))+1
+        randomDiceIndex2 = Int(arc4random_uniform(6))+1
+        print(randomDiceIndex1)
+
+    }
+    
 }
+
 
